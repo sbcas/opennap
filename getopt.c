5309e538db2cd041dca9a25b9651274667e68a51
drscholl <>
2000-03-01 22:43:04 +0000

    added getopt() replacement for Win32 which doesn't have this function

---
+++ getopt.c
@@ -0,0 +1,41 @@
+#ifdef WIN32
+#include <windows.h>
+#endif
+
+#define EOF -1
+
+int optind = 1;
+char *optarg;
+
+/* -b 500 -v */
+int
+_getopt (int ac, char **av, const char *opts)
+{
+    const char *p;
+
+    while (optind < ac)
+    {
+	if (*av[optind] == '-')
+	{
+	    p = strchr (opts, av[optind][1]);
+	    if (!p)
+		return '?';
+	    if (*(p+1) == ':')
+	    {
+		/* requires arg */
+		optind++;
+		if (!av[optind] || *av[optind] == '-')
+		    return ':'; /* missing argument */
+		optarg = av[optind];
+	    }
+	    else
+		optarg = 0;
+	    optind++;
+	    return *p;
+	}
+	else
+	    break;
+    }
+    optarg = 0;
+    return EOF;
+}
