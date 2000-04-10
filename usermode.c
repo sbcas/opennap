92a7c2eb16ea3a72d43d804d794a3d4c8b355d5d
drscholl <>
2000-04-10 20:11:44 +0000

    added new function handler user_mode()

---
+++ usermode.c
@@ -0,0 +1,84 @@
+/* Copyright (C) 2000 edwards@bitchx.dimension6.com
+   This is free software distributed under the terms of the
+   GNU Public License.  See the file COPYING for details.
+
+   $Id$ */
+
+/*
+ * written by Colten Edwards.
+ */
+
+#include <string.h>
+#include "opennap.h"
+#include "debug.h"
+
+static char *User_Levels[] = { "ERROR", "BAN", "CHANGE", "CHANNEL",
+    "KILL", "LEVEL", "SERVER", "MUZZLE", ""
+};
+
+
+/* 10203 [mode] */
+HANDLER (user_mode)
+{
+    USER *sender;
+    int neg = 0, i, p;
+    unsigned int level = 0;
+    char *av;
+
+    CHECK_USER_CLASS ("user_mode");
+    ASSERT (validate_connection (con));
+    (void) len;
+    if (pop_user (con, &pkt, &sender))
+	return;
+    if (!pkt || !*pkt)
+    {
+	char buffer[250];
+	int buflen;
+
+	if (sender->con->uopt->usermode == 0)
+	    strcpy (buffer, "NONE");
+	else
+	{
+	    buffer[0] = 0;
+	    for (i = 0, p = 1; *User_Levels[i]; i++, p <<= 1)
+	    {
+		if (sender->con->uopt->usermode & p)
+		{
+		    buflen = strlen (buffer);
+		    snprintf (buffer + buflen, sizeof (buffer) - buflen, "%s%s",
+			    buflen>0?" ":"", User_Levels[i]);
+		}
+	    }
+	}
+	send_cmd (con, MSG_SERVER_USER_MODE, "%s", buffer);
+	return;
+    }
+    while ((av = next_arg (&pkt)))
+    {
+	if (!strcasecmp (av, "ALL"))
+	    level = LOGALL_MODE;
+	else if (!strcasecmp (av, "NONE"))
+	    level = 0;
+	else if (*av == '-')
+	{
+	    neg = 1;
+	    av++;
+
+	}
+	else
+	    neg = 0;
+	for (i = 0, p = 1; *User_Levels[i]; i++, p <<= 1)
+	{
+	    if (!strcasecmp (av, User_Levels[i]))
+	    {
+		if (neg)
+		    level &= (LOGALL_MODE ^ p);
+		else
+		    level |= p;
+		break;
+	    }
+	}
+    }
+    if (sender->con->uopt->usermode != level)
+	sender->con->uopt->usermode = level;
+}
